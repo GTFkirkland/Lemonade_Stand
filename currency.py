e2bd1cf36e847f8b5ea5ccd36c52d6dbcfc13878
GTFkirkland <mk536875@georgetownisd.org>
2026-01-22 15:26:15 -0600

    Added beta versions

---
+++ currency.py
@@ -0,0 +1,4 @@
+#to use this function you need to use the amount and current money
+def changeMoney(currentMoney, amount):
+    currentMoney += amount
+    return currentMoney
---
+++ lemonade_stand_run.py
@@ -0,0 +1,11 @@
+#imports
+import currency
+
+#Money stuff
+    #use money function with changeMoney(current amount of money(money) and change amount(- or +))
+    #example change:
+    #money = currency.changeMoney(money, -12)
+money = 300 #starting money
+
+money = currency.changeMoney(money, -12)
+print(money)
