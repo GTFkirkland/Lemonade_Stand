11612b9f7e92cec1c8206d2d0c7688b0d50acc21
GTFkirkland <mk536875@georgetownisd.org>
2026-01-23 10:10:07 -0600

    Added more comments

--- lemonade_stand_run.py
+++ lemonade_stand_run.py
@@ -1,11 +1,10 @@
 #imports
 import currency
-
+#run with cd ./Lemonade_Stand 
 #Money stuff
     #use money function with changeMoney(current amount of money(money) and change amount(- or +))
     #example change:
     #money = currency.changeMoney(money, -12)
 money = 300 #starting money
 
-money = currency.changeMoney(money, -12)
-print(money)
+ 
