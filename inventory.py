8b922fc980069e39a92257dc2004f04ff3176147
LoganPartida <lp539237@georgetownisd.org>
2026-01-28 15:26:40 -0600

    fixed inventory

--- inventory.py
+++ inventory.py
@@ -8,26 +8,26 @@ hotSauce = 0
 def buyIngre(money):
     item = input("What do you want to buy?(shells/meat/cheese/hot sauce)\n")
     amount = int(input("How many of this item?\n"))
-    inventory(item, amount, money)
+    return inventory(item, amount, money)
 
 def inventory(item, amount, money):
     global shells
     global meat
     global cheese
     global hotSauce
-    if item == shells:
+    if item == "shells":
         if money >= amount * .10:
             shells += amount
             money -= amount * .10
         else:
             print("You're too poor to afford this.")
-    elif item == meat:
+    elif item == "meat":
         if money >= amount * .30:
             meat += amount
             money -= amount * .30
         else:
             print("You're to poor to afford this.")
-    elif item == cheese:
+    elif item == "cheese":
         if money >= amount * .20:
             cheese += amount
             money -= amount * .20
