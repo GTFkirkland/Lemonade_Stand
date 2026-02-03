739e41cbcc1a90cd8da8b8c601860ce7f1992789
GTFkirkland <mk536875@georgetownisd.org>
2026-02-03 15:24:17 -0600

    finished price ranges

--- pricing.py
+++ pricing.py
@@ -1,17 +1,15 @@
 #This is how the player will set pricing for their game.
-
-price = 0 
-
 def pricingFull():
     price = 0
-    try:
-        price = float(input("What do you want to set your price as? \n"))
-        if price < 0:
-            None
-    except:
-        print("This is an INVALID price, please try again")
-        pricingFull()
-    print(f"Your price is {price}")
+    while price <= 0 or price > 5:
+        try:
+            price = float(input("What do you want to set your price as? \n"))
+            if price <= 0 or price > 5:
+                print("This is an INVALID price, please try again")
+        except:
+            print("This is an INVALID price, please try again")
+            continue
+    print(f"Your price is ${price}")
     check = input("Do you like these changes?(y/n)\n")
     if check == "no" or check == "No" or check == "n" or check == "N":
         print("Reverting changes... RELOADING")
