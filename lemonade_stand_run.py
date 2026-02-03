562c81a9211592b3177a0417c24864b0798e5d7a
GTFkirkland <mk536875@georgetownisd.org>
2026-02-03 10:21:14 -0600

    added stuff (NO TIME)

--- lemonade_stand_run.py
+++ lemonade_stand_run.py
@@ -1,15 +1,13 @@
-import currency
 import inventory
+import pricing
+import recipes
 money = 300 #starting money(temp)
 #folder- cd ./Lemonade_Stand
-#imports
-#Money stuff
-    #use money function with changeMoney(current amount of money(money) and change amount(- or +))
-    #example change:
-    #money = currency.changeMoney(money, -12)
 #inventory functions
     #Use buyIngre(money) to buy ingrediants
     #Use showStat(money) to show inventory and money
+#recipe
+    #use changeRecipe(recipe)
 
 
 
--- pricing.py
+++ pricing.py
@@ -2,10 +2,29 @@
 
 price = 0 
 
+def pricingFull():
+    price = 0
+    try:
+        price = float(input("What do you want to set your price as? \n"))
+        if price < 0:
+            None
+    except:
+        print("This is an INVALID price, please try again")
+        pricingFull()
+    print(f"Your price is {price}")
+    check = input("Do you like these changes?(y/n)\n")
+    if check == "no" or check == "No" or check == "n" or check == "N":
+        print("Reverting changes... RELOADING")
+        print("~~~~~~~~~~~~~~~~~~~~~~~~")
+        pricingFull()
+    else:
+        print("Changes successfuly made...")
+        return price
+
 def pricing():
-    global price
-    price=int(input("What would you like to set your price as:"))
-    while price < 0:
-        print("This is an invalid price please try again...")  
+    print("~~~~~~~~~~~PRICE~~~~~~~~~~~~")
+    pricingFull()
+
+
 pricing()
    
