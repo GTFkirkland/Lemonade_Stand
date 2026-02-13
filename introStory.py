036f854cd560628861957af817ecfbb878bf776a
LoganPartida <lp539237@georgetownisd.org>
2026-02-13 14:57:12 -0600

    Milo changed something

--- introStory.py
+++ introStory.py
@@ -3,7 +3,7 @@ def startGame(style):
         print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~BACKGROUND~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
         check = input("You see the hand you've been dealt, and realize that you cant win. (press enter to continue)")
         check = input("As the depresion seeps in, you start imagining what you will do when you exit the casino.")
-        check = input("after losing $1,000, which is all the currency you've ever had, you spot an abandoned taco truck in an alleyway.")
+        check = input("after losing $1,000, which is all the currency you've ever had, you spot an abandoned taco truck in an alleyway")
         check = input("and began devising a plan...")
 
     else:
