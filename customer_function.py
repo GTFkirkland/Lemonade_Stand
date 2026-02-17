d6bfa245ef74dd799caaff40c478620a91a4a90e
GTFkirkland <mk536875@georgetownisd.org>
2026-02-17 10:22:58 -0600

    Added story things

--- customer_function.py
+++ customer_function.py
@@ -17,11 +17,11 @@ def runCustomers(recipe, price, ingredients):
         print(f"Customer {i + 1} walks up to the truck...")
 
         if price > price_limit:
-            print(f"Customer {i + 1} looked at the price and walked away. Too expensive")
+            print(f"Customer {i + 1} looked at the price and walked away. you can't monopolize your customers")
         elif abs(recipe["cheese"] - cheesy_pref) > 1:
-            print(f"Customer {i + 1} didnt like the cheese amount. They left")
+            print(f"Customer {i + 1} didnt like the cheese amount. They said they couldn't even see the shell")
         elif abs(recipe["hotSauce"] - spice_pref) > 1:
-            print(f"Customer {i + 1} didnt like the spice level. They left")
+            print(f"Customer {i + 1} didnt like the spice level. Your customer said they didn't want to be a fire breathing dragon")
         else:
             if ingredients["shells"] >= 1 and ingredients["meat"] >= recipe["meat"] and ingredients["cheese"] >= recipe["cheese"] and ingredients["hotSauce"] >= recipe["hotSauce"]:
                 print(f"Customer {i + 1} bought a taco! +${price}")
--- introStory.py
+++ introStory.py
@@ -2,9 +2,10 @@ def startGame(style):
     if style == 1:
         print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~BACKGROUND~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
         check = input("You see the hand you've been dealt, and realize that you cant win. (press enter to continue)")
-        check = input("As the depression seeps in, you start imagining what you will do when you exit the casino.")
+        check = input("As the depression seeps in, you start imagining what you will do when you depart from the casino.")
+        check = input("*Walking back to your tent beneath the underpass*")
         check = input("After losing $1,000, which is all the currency you've ever had, you spot an abandoned taco truck in an alleyway.")
-        check = input("Inside the truck you spot a MASSIVE amount of pennies on the floor")
+        check = input("Inside the truck you spot a ludicrous amount of pennies on the floor.")
         check = input("After collecting")
         check = input("EVERY")
         check = input("SINGLE")
