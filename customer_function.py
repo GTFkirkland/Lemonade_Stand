151cc9d26819fa033c710e4fabc71972ed93ceb9
LoganPartida <lp539237@georgetownisd.org>
2026-02-18 15:26:40 -0600

    Changed grammar issues

--- customer_function.py
+++ customer_function.py
@@ -19,9 +19,9 @@ def runCustomers(recipe, price, ingredients):
         if price > price_limit:
             print(f"Customer {i + 1} looked at the price and walked away. you can't monopolize your customers")
         elif abs(recipe["cheese"] - cheesy_pref) > 1:
-            print(f"Customer {i + 1} didnt like the cheese amount. They said they couldn't even see the shell")
+            print(f"Customer {i + 1} didn't like the cheese amount. They said they couldn't even see the shell")
         elif abs(recipe["hotSauce"] - spice_pref) > 1:
-            print(f"Customer {i + 1} didnt like the spice level. Your customer said they didn't want to be a fire breathing dragon")
+            print(f"Customer {i + 1} didn't like the spice level. Your customer said they didn't want to be a fire breathing dragon")
         else:
             if ingredients["shells"] >= 1 and ingredients["meat"] >= recipe["meat"] and ingredients["cheese"] >= recipe["cheese"] and ingredients["hotSauce"] >= recipe["hotSauce"]:
                 print(f"Customer {i + 1} bought a taco! +${price}")
